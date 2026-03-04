d12feedde8feca948c7efac5436910d84fa5f383
tortiman <josejavierbedmar@gmail.com>
2026-03-04 19:47:17 +0100

    Añadir validación de correo electrónico y estilos de error en el formulario de contacto

--- js/script.js
+++ js/script.js
@@ -3,7 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
     const btnClose=document.getElementById('btnClose');
     const menuMobile=document.getElementById('menuMobile');
     const menuHamburger=document.getElementById('menuHamburger');
+    const inputEmail=document.getElementById('inputEmail');
+    const errorEmail=document.getElementById('errorEmail');
+    const iconError=document.getElementById('iconError');
     
+
+    function checkEmail(email){
+        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        if(email==='' || regex.test(email)===false){
+            iconError.classList.remove('hidden');
+            inputEmail.classList.add('border-2');
+            inputEmail.classList.add('border-red-500');
+            errorEmail.classList.remove('hidden');
+
+        }else{
+            iconError.classList.add('hidden');
+            inputEmail.classList.remove('border-2');
+            errorEmail.classList.add('hidden');
+            inputEmail.classList.remove('border-red-500');
+        }
+    }
+
 
     btnHamburger.addEventListener('click', ()=>{ // ocultamos menuHamburger y mostramos menuMobile
 
@@ -17,5 +37,11 @@ document.addEventListener('DOMContentLoaded', function () {
         menuMobile.classList.add('hidden');
         menuHamburger.classList.remove('hidden');
     
+    });
+
+    inputEmail.addEventListener('keyup', (event)=>{
+        if(event.key==='Enter'){
+            checkEmail(inputEmail.value);
+        }
     });
 });
